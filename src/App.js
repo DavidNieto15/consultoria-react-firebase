97758af0500c2d7063bce0d5f2d29a5c885a9fb2
David Nieto Hernández <105694872+DavidNieto15@users.noreply.github.com>
2022-10-05 12:42:30 -0500

    Actualización rutas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-//import Navbar from "./components/navbar/Navbar";
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Footer from "./components/footer/Footer";
-//import Body from "./components/Body";
 import AppRouter from './routes/AppRouter';
 
 function App() {
